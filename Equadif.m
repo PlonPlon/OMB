b9780cc6a9202731ebabbd8a13c1de420d5dda72
PlonPlon <clement.violet@etudiant.univ-brest.fr>
2016-11-03 22:34:48 +0100

    Mise à jour de la fonction NPZ
    Changement de nom pour Equadif, mise à jour du code, ajout de commentaires et ajout du calcul du flux de croissance du Marsu et des variables associés.
    
    Le but de cette fonction est donc de calculer toutes les parties de équations différentielles pour ensuite les résoudre numériquement.

--- Equadif.m
+++ Equadif.m
@@ -1,6 +1,15 @@
-function [dNdt,dPdt,dZdt] = NPZ(N,P,Z)
+#########################   Fonction equations différentielles    #########################
 
-# Variables du modèle :
+#{
+Calcul les flux de différentes équations différentielles utilisée dans le modèle de production lacustre (NPZ) 
+et dans celui de croissance du Marsupilami.
+#}
+
+
+function [dNdt,dPdt,dZdt,dLdt] = Equadif(N,P,Z,L)
+
+# Variables du modèle NPZ :
+
 
 Vm = 1.0; #jour−^1 – taux maximum d’assimilation de l’azote par le phyton
 ks = 1.0; #mole.N.l^−1 – constante de demi-saturation de l’assimilation de l’azote par le phyton
@@ -12,12 +21,27 @@ gamma = 0.7; #(sans dimension) proportion d’azote assimilée par les Métynnis
 fI0 = 0.25; #fonction de l’intensité de la lumière (habituellement une décroissance expo-nentielle en fonction de la profondeur) ; dans le cas présent, la profondeur de l’étang est constante et on fera l’hypothèse, dans un premier temps au moins, que f (I 0 ) est constante et égale à 0.25Ò
 
 
-#Calcul de la fonction :
+
+# Variables du modèle de croissance du Marsu
+
+Linf = 120; #Longueur théorique Max du Marsu en cm
+
+K = 0.001; # Constante de croissance en jour^-1
+
+
+
+#Calcul du modèle NPZ:
 
 dNdt = -(Vm*N)/(ks+N)*fI0*P+(1-gamma)*Z*Rm*(1-exp(-lambda*P))+e*P+g*Z; #Taux d'accroissement en azote
 
 dPdt = (Vm*N)/(ks+N)*fI0*P-Z*Rm*(1-exp(-lambda*P))-e*P; #Taux d'accroisssement d'algue
 
-dZdt = gamma*Z*Rm*(1-exp(-lambda*P))-g*Z; #Taux d'accrissement en Métynnis
+dZdt = gamma*Z*Rm*(1-exp(-lambda*P))-g*Z; #Taux d'accroissement en Métynnis
+
+
+
+# Calcul du modèle de croissance marsupilami
+
+dLdt = K*(Linf*Z - L);
 
 end

659892447b562039a054b4cec5bda9432226a1c6
PlonPlon <clement.violet@etudiant.univ-brest.fr>
2016-11-05 21:13:15 +0100

    Mise à jour
    Script à nouveau fonctionnel

--- EcosysMarsu_Euler.m
+++ EcosysMarsu_Euler.m
@@ -1,18 +1,21 @@
 clear all, close all
 
 #Initialisation :
-#       Partie commune :
 
-t0 = 0;
-tf = 365*1;
+#  Variables communes :
+
+t0 = 1;
+tf = 365*4;
 dt = 0.1;
 n = tf/dt;
 t = t0;
 T = t; #Matrice dans laquelle on stocke tous les temps
 
-#                                  Modèle NPZ
 
-N0 = 4; #μ.mole.N.l.^-1
+# Modèle NPZ
+
+
+N0 = 4; #μ.mole.N.l.^-1 
 P0 = 2.5; #μ.mole.N.l.^-1
 Z0 = 0.5; #μ.mole.N.l.^-1
 
@@ -25,22 +28,13 @@ Psol = P; #Matrice dans laquelle on stocke toutes les solutions de P
 Zsol = Z; #Matrice dans laquelle on stocke toutes les solutions de Z
 
 
+# Modèle croissance Marsu
 
-
-#                             Modèle croissance Marsu
 L0 = 7; #cm 
 
 L = L0; #Taille du Marsupilami (en cm) et à T0
 
 Lsol = L; #Matrice dans laquelle on stocke toutes les solutions de L
-
-
-
-
-
-#                     Résolution numérique des équations difféntiellelles
-#                             par la méthode d'Euler
-
 
 
 for i = 2:n #On commence la boucle à 2 car la valeur du jour 1 est celui à t = 0
@@ -49,7 +43,7 @@ for i = 2:n #On commence la boucle à 2 car la valeur du jour 1 est celui à t =
   
   [dNdt,dPdt,dZdt,dLdt] = Equadif(N,P,Z,L)
   
-  ##         Résolution des équations du modèle NPZ
+  # Résolution des équations du modèle NPZ
   
   
   N = N + dNdt*dt; #On calcul les solutions de N,P,Z pour chaque valeur de dt
@@ -59,13 +53,12 @@ for i = 2:n #On commence la boucle à 2 car la valeur du jour 1 est celui à t =
   
   
   
-  ##         Résolution de l'équation différentielle du modèle de croissance du Marsu
+  # Résolution de l'équation différentielle du modèle de croissance du Marsu
   
   
-  if dLdt < 0 #Si le flux de croissance du Marsu est inférieur à 0, il devient nul.
-    #          En effet, notre Marsu ne peut pas raptisser !
-     dLdt = 0;
-   
+  if dLdt < 0 #Si le flux de croissance du Marsu est inférieur à 0, il devient nul. En effet, notre Marsu ne peut pas raptisser !
+     dLdt = 0;  
+     
   end
   
   L = L + dLdt*dt;
@@ -82,22 +75,30 @@ end
 
 
 
-#                             Graphiques
+# Graphiques
 
 
 
-figure(1) #Toutes les courbes sur un même graphique
+figure(1) 
 
-plot(T,Nsol,'b',T,Psol,'g',T,Zsol,'r',T,Lsol,'k')
-legend('Azote','Algue','Metynnis','Marsu')
+#subplot(2,1,1)
+
+plot(T,Nsol,'b',T,Psol,'g',T,Zsol,'r') # Toutes les courbes sur un même graphique
+legend('Azote','Algue','Metynnis')
 xlabel('Temps (jour)')
 ylabel('Concentration en Azote (\mumole N.l.^{-1})')
 grid on
 
-figure(2)
 
-plot(T,Lsol,'k',T,Zsol,'r')
-legend('Marsu','Metynnis')
-xlabel('Temps (jour)')
-ylabel('Concentration en Azote (\mumole N.l.^{-1})')
+#subplot(2,1,2)
+figure (2)
+[hax,h1,h2] = plotyy(T,Lsol,T,Zsol);
+set(hax(1),'ycolor','k');
+set(hax(2),'ycolor','r');
+set (h1, 'color','k','linestyle','-');
+set (h2, 'color','r');
+legend('Marsu','Metynnis');
+xlabel('Temps (jour)');
+ylabel(hax(1),'Taillen (cm)');
+ylabel(hax(2),'Concentration en Azote (\mumole N.l.^{-1})');
 grid on

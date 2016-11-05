5de1e02545f392ec21586b609cdea7f1b9a28e51
PlonPlon <clement.violet@etudiant.univ-brest.fr>
2016-11-05 22:26:35 +0100

    Mise à jour
    Script de nouveau fonctionnel

--- EcosysMarsu_RK.m
+++ EcosysMarsu_RK.m
@@ -1,18 +1,21 @@
 clear all, close all
 
 #Initialisation :
-#       Partie commune :
+
+#  Variables communes :
 
 t0 = 1;
-tf = 365*1;
+tf = 365*4;
 dt = 0.1;
 n = tf/dt;
 t = t0;
 T = t; #Matrice dans laquelle on stocke tous les temps
 
-#                                  Modèle NPZ
 
-N0 = 4; #μ.mole.N.l.^-1
+# Modèle NPZ
+
+
+N0 = 4; #μ.mole.N.l.^-1 
 P0 = 2.5; #μ.mole.N.l.^-1
 Z0 = 0.5; #μ.mole.N.l.^-1
 
@@ -25,22 +28,13 @@ Psol = P; #Matrice dans laquelle on stocke toutes les solutions de P
 Zsol = Z; #Matrice dans laquelle on stocke toutes les solutions de Z
 
 
+# Modèle croissance Marsu
 
-
-#                             Modèle croissance Marsu
 L0 = 7; #cm 
 
 L = L0; #Taille du Marsupilami (en cm) et à T0
 
 Lsol = L; #Matrice dans laquelle on stocke toutes les solutions de L
-
-
-
-
-
-#                     Résolution numérique des équations difféntiellelles
-#                            par la méthode de Runge-Kutta
-
 
 
 for i = 2:n #On commence la boucle à 2 car la valeur du jour 1 est celui à t = 0
@@ -52,36 +46,28 @@ for i = 2:n #On commence la boucle à 2 car la valeur du jour 1 est celui à t =
   [NRK3,PRK3,ZRK3,LRK3] = Equadif(1/2*dt*NRK2,1/2*dt*PRK2,1/2*dt*ZRK2,1/2*dt*LRK2);
   [NRK4,PRK4,ZRK4,LRK4] = Equadif(dt*NRK3,dt*PRK3,dt*ZRK3,dt*LRK3);
   
+  
   dNdt = 1/6*NRK1 + 1/3*NRK2 + 1/3*NRK3 + 1/6*NRK4; 
   dPdt = 1/6*PRK1 + 1/3*PRK2 + 1/3*PRK3 + 1/6*PRK4;
   dZdt = 1/6*ZRK1 + 1/3*ZRK2 + 1/3*ZRK3 + 1/6*ZRK4;
   
-  
-  ##         Résolution des équations du modèle NPZ
+  # Résolution des équations du modèle NPZ
   
   
   N = N + dt*dNdt;    #On calcule les solutions de N,P,Z pour chaque valeur de dt
   P = P + dt*dPdt;
   Z = Z + dt*dZdt;
   
+  # Résolution de l'équation différentielle du modèle de croissance du Marsu
   
   
-  ##         Résolution de l'équation différentielle du modèle de croissance du Marsu
-  
-  
-  if dLdt < 0 #Si le flux de croissance du Marsu est inférieur à 0, il devient nul.
-    #          En effet, notre Marsu ne peut pas raptisser !
-     dLdt = 0;
-   
+  if dLdt < 0 #Si le flux de croissance du Marsu est inférieur à 0, il devient nul. En effet, notre Marsu ne peut pas raptisser !
+     dLdt = 0;  
+     
   end
-  
   
   L = L + dLdt*dt;
   
-  
-  
-  
-  ##          Stockage de toutes les solutions
   
   Nsol = [Nsol ; N]; #On stocke toutes les solutions de N,P,Z,L dans des matrices
   Psol = [Psol ; P];
@@ -94,4 +80,30 @@ end
 
 
 
-#                             Graphiques
+# Graphiques
+
+
+
+figure(1) 
+
+#subplot(2,1,1)
+
+plot(T,Nsol,'b',T,Psol,'g',T,Zsol,'r') # Toutes les courbes sur un même graphique
+legend('Azote','Algue','Metynnis')
+xlabel('Temps (jour)')
+ylabel('Concentration en Azote (\mumole N.l.^{-1})')
+grid on
+
+
+#subplot(2,1,2)
+figure (2)
+[hax,h1,h2] = plotyy(T,Lsol,T,Zsol);
+set(hax(1),'ycolor','k');
+set(hax(2),'ycolor','r');
+set (h1, 'color','k','linestyle','-');
+set (h2, 'color','r');
+legend('Marsu','Metynnis');
+xlabel('Temps (jour)');
+ylabel(hax(1),'Taillen (cm)');
+ylabel(hax(2),'Concentration en Azote (\mumole N.l.^{-1})');
+grid on
